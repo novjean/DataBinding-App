7e68e55c44f44eb6a319d447cbdd963f3daf874e
Novjean J Kannathara <novjeanj@gmail.com>
2024-05-08 23:52:06 +0530

    Initial commit

---
+++ app/src/main/java/com/novatech/databindingapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.novatech.databindingapp
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.novatech.databindingapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        // not needed when using data bingin
+        //setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+//        binding.btn.setOnClickListener(){
+//            var enteredText :String = binding.editText.text.toString()
+//            binding.textView.setText("hello $enteredText")
+//        }
+
+
+        // use this so as to not have to write binding everywhere
+        binding.apply {
+            btn.setOnClickListener(){
+                var enteredText :String = editText.text.toString()
+                textView.setText("hello $enteredText")
+            }
+        }
+    }
+}
